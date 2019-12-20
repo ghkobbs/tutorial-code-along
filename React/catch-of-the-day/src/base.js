8d488bef7e90b5f2ea0d6fcac6f5943d0c6ab97d
unknown <maxkobbs@gmail.com>
2019-12-20 07:11:30 +0000

    Update: Added firebase auth details

--- React/catch-of-the-day/src/base.js
+++ React/catch-of-the-day/src/base.js
@@ -1,5 +1,9 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-    
+    apiKey: "AIzaSyAsw9v4cSRXum8R55kgBidRlqeQs8dCKBg",
+    authDomain: "catch-of-the-day-max-279e8.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-max-279e8.firebaseio.com",
 });
+
+export default base;
